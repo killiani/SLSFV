aac3e345f0f9005703d5e8d0ebf25295396e67bf
Ben <killiani@yahoo.de>
2020-03-26 16:37:39 +0100

    Menu anpassungen, respo

--- templates/mobile-menu.php
+++ templates/mobile-menu.php
@@ -6,3 +6,32 @@
     <img src="files/assets/icons/cross.svg" data-mobile-menu="close" href="#">
 </div>
 
+<div class="nav_punkte">
+
+    <div class="logo-mobile-nav">
+        <a  href="{{link_url::3}}"><img src="files/assets/icons/09.svg" width="200px"></a>
+    </div>
+    <ul>
+        <li class="nav_punkte_einzeln"><a  class="main_nav_punkt" href="{{link_url::3}}">Aktuelles</a> </li>
+        <li class="nav_punkte_einzeln"><a  class="main_nav_punkt" href="{{link_url::4}}">SLSFV</a> </li>
+            <ul>
+                <li class="nav_punkte_einzeln"><a  class="scroll-button" href="{{link_url::12}}">Über Uns</a> </li>
+                <li class="nav_punkte_einzeln"><a  class="scroll-button" href="{{link_url::13}}">Mitglied werden</a> </li>
+                <li class="nav_punkte_einzeln"><a  class="scroll-button" href="{{link_url::14}}">Vereinshaftspflichtversicherung</a> </li>
+                <li class="nav_punkte_einzeln"><a  class="scroll-button" href="{{link_url::15}}">Vereinsatzung</a> </li>
+                <li class="nav_punkte_einzeln"><a  class="scroll-button" href="{{link_url::16}}">Handbuch für Kita- & Schulfördervereine</a> </li>
+            </ul>
+        <li class="nav_punkte_einzeln"><a  class="main_nav_punkt" href="{{link_url::5}}">Rund um Fördervereine</a> </li>
+            <ul>
+                <li class="nav_punkte_einzeln"><a  class="scroll-button" href="{{link_url::17}}">Nützliches zum Thema Fördervereine</a> </li>
+                <li class="nav_punkte_einzeln"><a  class="scroll-button" href="{{link_url::18}}">Fördermöglichkeiten</a> </li>
+                <li class="nav_punkte_einzeln"><a  class="scroll-button" href="{{link_url::19}}">andere Landesverbände</a> </li>
+                <li class="nav_punkte_einzeln"><a  class="scroll-button" href="{{link_url::20}}">Best Practice Sammlung</a> </li>
+            </ul>
+        <li class="nav_punkte_einzeln"><a  class="main_nav_punkt" href="{{link_url::6}}">Chancenpatenschaften</a> </li>
+            <ul>
+                <li class="nav_punkte_einzeln"><a  class="scroll-button" href="{{link_url::22}}">Geschichten aus den Chancenpatenschaften</a> </li>
+            </ul>
+        <li class="nav_punkte_einzeln"><a  class="main_nav_punkt" href="{{link_url::7}}">Spenden</a> </li>
+    </ul>
+</div>
